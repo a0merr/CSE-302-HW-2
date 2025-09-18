3aec01010b1cc97b7c61d3e51ff86758500471eb
Andrew Merritt <a0merr05@louisville.edu>
2025-09-18 11:24:47 -0400

    Stringdecoding.cpp

--- Stringdecoding.cpp
+++ Stringdecoding.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include "StackType.h"
 #include "QueType.h"
+
 std::string decode(std::string exp, std::string code) {
-    QueType<char> normalQueue; 
-    StackType<char> codeStack;  
+    QueType<char> normalQueue;
+    StackType<char> codeStack;
+
     for (int i = 0; i < static_cast<int>(exp.length()); i++) {
         char ch = exp.at(i);
         if (code.find(ch) == std::string::npos) {
@@ -12,16 +14,21 @@ std::string decode(std::string exp, std::string code) {
             codeStack.Push(ch);
         }
     }
+
     std::string output = "";
+
     while (!normalQueue.IsEmpty()) {
         char ch;
         normalQueue.Dequeue(ch);
         output += ch;
     }
+
     while (!codeStack.IsEmpty()) {
         char ch;
         codeStack.Pop(ch);
         output += ch;
     }
+
     return output;
 }
+
